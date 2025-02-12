f99906e3b5c978c456640ebbe77779cce675115f
Ian Lambert <ian.lambert@kellogg.northwestern.edu>
2025-02-12 21:20:22 +0000

    Inserted the roles

--- kmdb.rb
+++ kmdb.rb
@@ -90,7 +90,8 @@ Role.destroy_all
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
 # TODO!
-
+# ==================================================================
+# Studio
 puts "Studios: #{Studio.all.count}"
 
 new_studio = Studio.new
@@ -100,7 +101,8 @@ new_studio.save
 puts new_studio.inspect
 puts "Studios: #{Studio.all.count}"
 # ==================================================================
-
+# Movie
+# studio variables:
 warner_bro = Studio.find_by({ "name" => "Warner Bros." })
 
 puts "Movies: #{Movie.all.count}"
@@ -131,6 +133,207 @@ puts new_movie1.inspect
 puts new_movie2.inspect
 
 puts "Movies: #{Movie.all.count}"
+
+# ==================================================================
+# Actor
+
+new_actor1 = Actor.new
+new_actor1["name"] = "Christian Bale"
+new_actor1.save
+
+new_actor2 = Actor.new
+new_actor2["name"] = "Michael Caine"
+new_actor2.save
+
+new_actor3 = Actor.new
+new_actor3["name"] = "Liam Neeson"
+new_actor3.save
+
+new_actor4 = Actor.new
+new_actor4["name"] = "Katie Holmes"
+new_actor4.save
+
+new_actor5 = Actor.new
+new_actor5["name"] = "Gary Oldman"
+new_actor5.save
+
+new_actor6 = Actor.new
+new_actor6["name"] = "Gary Oldman"
+new_actor6.save
+
+new_actor7 = Actor.new
+new_actor7["name"] = "Heath Ledger"
+new_actor7.save
+
+new_actor8 = Actor.new
+new_actor8["name"] = "Aaron Eckhart"
+new_actor8.save
+
+new_actor9 = Actor.new
+new_actor9["name"] = "Maggie Gyllenhaal"
+new_actor9.save
+
+new_actor10 = Actor.new
+new_actor10["name"] = "Tom Hardy"
+new_actor10.save
+
+new_actor11 = Actor.new
+new_actor11["name"] = "Anne Hathaway"
+new_actor11.save
+
+new_actor6["name"] = "Joseph Gordon-Levitt"
+new_actor6.save
+
+puts new_actor1.inspect 
+puts new_actor2.inspect 
+puts new_actor3.inspect 
+puts new_actor4.inspect 
+puts new_actor5.inspect 
+puts new_actor6.inspect 
+puts new_actor7.inspect 
+puts new_actor8.inspect 
+puts new_actor9.inspect 
+puts new_actor10.inspect 
+puts new_actor11.inspect 
+
+puts "Actors: #{Actor.all.count}"
+
+# ==================================================================
+# Role
+# moveie variables:
+batman_begins = Movie.find_by({ "title" => "Batman Begins" })
+the_dark_knight = Movie.find_by({ "title" => "The Dark Knight" })
+the_dark_knight_rises = Movie.find_by({ "title" => "The Dark Knight Rises" })
+
+# actore variables:
+christian_bale = Actor.find_by({ "name" => "Christian Bale"})
+michael_caine = Actor.find_by({ "name" => "Michael Caine"})
+liam_neeson = Actor.find_by({ "name" => "Liam Neeson"})
+katie_holmes = Actor.find_by({ "name" => "Katie Holmes"})
+gary_oldman = Actor.find_by({ "name" => "Gary Oldman"})
+heath_ledger = Actor.find_by({ "name" => "Heath Ledger"})
+aaron_eckhart = Actor.find_by({ "name" => "Aaron Eckhart"})
+maggie_gyllenhaal = Actor.find_by({ "name" => "Maggie Gyllenhaal"})
+tom_hardy = Actor.find_by({ "name" => "Tom Hardy"})
+joseph_gordon_levitt = Actor.find_by({ "name" => "Joseph Gordon-Levitt"})
+anne_hathaway = Actor.find_by({ "name" => "Anne Hathaway"})
+
+# Batman Begins
+new_role1 = Role.new
+new_role1["movie_id"] = batman_begins["id"]
+new_role1["actor_id"] = christian_bale["id"]
+new_role1["character_name"] = "Bruce Wayne"
+new_role1.save
+
+new_role2 = Role.new
+new_role2["movie_id"] = batman_begins["id"]
+new_role2["actor_id"] = michael_caine["id"]
+new_role2["character_name"] = "Alfred"
+new_role2.save
+
+new_role3 = Role.new
+new_role3["movie_id"] = batman_begins["id"]
+new_role3["actor_id"] = liam_neeson["id"]
+new_role3["character_name"] = "Ra's Al Ghul"
+new_role3.save
+
+new_role4 = Role.new
+new_role4["movie_id"] = batman_begins["id"]
+new_role4["actor_id"] = katie_holmes["id"]
+new_role4["character_name"] = "Rachel Dawes"
+new_role4.save
+
+new_role5 = Role.new
+new_role5["movie_id"] = batman_begins["id"]
+new_role5["actor_id"] = gary_oldman["id"]
+new_role5["character_name"] = "Commissioner Gordon"
+new_role5.save
+
+# The Dark Knight
+
+new_role6 = Role.new
+new_role6["movie_id"] = the_dark_knight["id"]
+new_role6["actor_id"] = christian_bale["id"]
+new_role6["character_name"] = "Bruce Wayne"
+new_role6.save
+
+new_role7 = Role.new
+new_role7["movie_id"] = the_dark_knight["id"]
+new_role7["actor_id"] = heath_ledger["id"]
+new_role7["character_name"] = "Joker"
+new_role7.save
+
+new_role8 = Role.new
+new_role8["movie_id"] = the_dark_knight["id"]
+new_role8["actor_id"] = aaron_eckhart["id"]
+new_role8["character_name"] = "Harvey Dent"
+new_role8.save
+
+new_role9 = Role.new
+new_role9["movie_id"] = the_dark_knight["id"]
+new_role9["actor_id"] = michael_caine["id"]
+new_role9["character_name"] = "Alfred"
+new_role9.save
+
+new_role10 = Role.new
+new_role10["movie_id"] = the_dark_knight["id"]
+new_role10["actor_id"] = maggie_gyllenhaal["id"]
+new_role10["character_name"] = "Rachel Dawes"
+new_role10.save
+
+# The Dark Knigh Rises
+new_role11 = Role.new
+new_role11["movie_id"] = the_dark_knight_rises["id"]
+new_role11["actor_id"] = christian_bale["id"]
+new_role11["character_name"] = "Bruce Wayne"
+new_role11.save
+
+new_role12 = Role.new
+new_role12["movie_id"] = the_dark_knight_rises["id"]
+new_role12["actor_id"] = gary_oldman["id"]
+new_role12["character_name"] = "Commissioner Gordon"
+new_role12.save
+
+new_role13 = Role.new
+new_role13["movie_id"] = the_dark_knight_rises["id"]
+new_role13["actor_id"] = tom_hardy["id"]
+new_role13["character_name"] = "Bane"
+new_role13.save
+
+new_role14 = Role.new
+new_role14["movie_id"] = the_dark_knight_rises["id"]
+new_role14["actor_id"] = joseph_gordon_levitt["id"]
+new_role14["character_name"] = "John Blake"
+new_role14.save
+
+new_role15 = Role.new
+new_role15["movie_id"] = the_dark_knight_rises["id"]
+new_role15["actor_id"] = anne_hathaway["id"]
+new_role15["character_name"] = "Selina Kyle"
+new_role15.save
+
+
+
+
+puts new_role1.inspect
+puts new_role2.inspect
+puts new_role3.inspect
+puts new_role4.inspect
+puts new_role5.inspect
+puts new_role6.inspect
+puts new_role7.inspect
+puts new_role8.inspect
+puts new_role9.inspect
+puts new_role10.inspect
+puts new_role11.inspect
+puts new_role12.inspect
+puts new_role13.inspect
+puts new_role14.inspect
+puts new_role15.inspect
+
+puts "Roles: #{Role.all.count}"
+
+
 
 # Prints a header for the movies output
 puts "Movies"
